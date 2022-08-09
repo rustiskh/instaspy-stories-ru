dd0e7006c2a21200a158ecf0dc080aca945601fa
Rustam <rustam1222@gmail.com>
2022-08-09 22:07:26 +0200

    stories page modal not adaptive

--- js/user-stories-page.js
+++ js/user-stories-page.js
@@ -32,4 +32,40 @@ $(document).ready(function () {
 		console.log(textContent);
 		videoSliderBtn.textContent = textContent;
 	});
+
+	// modal wrapper script
+
+	$(".modal-wrapper").slick({
+		variableWidth: true,
+		centerMode: true,
+		slidesToShow: 1,
+		dots: true,
+		prevArrow:
+			'<button type="button" class="slick-prev"><img src="icons/icon-arrow-left.svg"></button>',
+		nextArrow:
+			'<button type="button" class="slick-next"><img src="icons/icon-arrow-right.svg"></button>',
+	});
+
+	var videoSliderBtns = document.querySelectorAll(
+		".modal-wrapper .slick-dots button"
+	);
+	console.log(videoSliderBtns);
+	videoSliderBtns.forEach(function (videoSliderBtn) {
+		var textContent =
+			videoSliderBtn.textContent + " / " + videoSliderBtns.length;
+		console.log(textContent);
+		videoSliderBtn.textContent = textContent;
+	});
+
+	// open-close modal
+
+	$(".modal-body__close").on("click", function () {
+		$(".modal-stories-view").fadeOut("fast");
+	});
+
+	$(".stories-card .stories-card__video").each(function (i) {
+		$(this).on("click", function () {
+			$(".modal-stories-view").fadeIn("fast");
+		});
+	});
 });
