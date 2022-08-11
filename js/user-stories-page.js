37e427541aada8e5e82a6dbea0c7c821a19132bd
Rustam <rustam1222@gmail.com>
2022-08-11 15:28:48 +0200

    layout fixes (delete modal variant)

--- js/user-stories-page.js
+++ js/user-stories-page.js
@@ -32,42 +32,4 @@ $(document).ready(function () {
 		console.log(textContent);
 		videoSliderBtn.textContent = textContent;
 	});
-
-	// modal wrapper script
-
-	$(".modal-wrapper").slick({
-		variableWidth: true,
-		centerMode: true,
-		slidesToShow: 1,
-		dots: true,
-		prevArrow:
-			'<button type="button" class="slick-prev"><img src="icons/icon-arrow-left.svg"></button>',
-		nextArrow:
-			'<button type="button" class="slick-next"><img src="icons/icon-arrow-right.svg"></button>',
-	});
-
-	var videoModalSliderBtns = document.querySelectorAll(
-		".modal-wrapper .slick-dots button"
-	);
-	console.log(videoSliderBtns);
-	videoModalSliderBtns.forEach(function (videoModalSliderBtn) {
-		var textContent =
-			videoModalSliderBtn.textContent +
-			" / " +
-			videoModalSliderBtns.length;
-		console.log(textContent);
-		videoModalSliderBtn.textContent = textContent;
-	});
-
-	// open-close modal
-
-	$(".modal-body__close").on("click", function () {
-		$(".modal-stories-view").fadeOut("fast");
-	});
-
-	$(".stories-card .stories-card__video").each(function (i) {
-		$(this).on("click", function () {
-			$(".modal-stories-view").fadeIn("fast");
-		});
-	});
 });
