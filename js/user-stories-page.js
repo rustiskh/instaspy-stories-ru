6b24429d8788100f52eb204450aaf7e971fc9149
Rustam <rustam1222@gmail.com>
2022-08-16 12:12:39 +0200

    seo-text replace

--- js/user-stories-page.js
+++ js/user-stories-page.js
@@ -12,23 +12,17 @@ $(document).ready(function () {
 			{
 				breakpoint: 740,
 				settings: {
-					centerPadding: "30px",
-					prevArrow:
-						'<button type="button" class="slick-prev"><img src="icons/icon-arrow-left.svg"></button>',
-					nextArrow:
-						'<button type="button" class="slick-next"><img src="icons/icon-arrow-right.svg"></button>',
+					prevArrow: '<button type="button" class="slick-prev"><img src="icons/icon-arrow-left.svg"></button>',
+					nextArrow: '<button type="button" class="slick-next"><img src="icons/icon-arrow-right.svg"></button>',
 				},
 			},
 		],
 	});
 
-	var videoSliderBtns = document.querySelectorAll(
-		".user-stories-wrapper .slick-dots button"
-	);
+	var videoSliderBtns = document.querySelectorAll(".user-stories-wrapper .slick-dots button");
 	console.log(videoSliderBtns);
 	videoSliderBtns.forEach(function (videoSliderBtn) {
-		var textContent =
-			videoSliderBtn.textContent + " / " + videoSliderBtns.length;
+		var textContent = videoSliderBtn.textContent + " / " + videoSliderBtns.length;
 		console.log(textContent);
 		videoSliderBtn.textContent = textContent;
 	});
