47236e4c957061213c62e7dd96256db907ac7576
Rustam <rustam1222@gmail.com>
2022-08-16 15:35:32 +0200

    menu position: fixed

--- js/script.js
+++ js/script.js
@@ -3,30 +3,19 @@ window.addEventListener("DOMContentLoaded", () => {
 	const menu = document.querySelector(".menu"),
 		menuItem = document.querySelectorAll(".menu__link"),
 		hamburger = document.querySelector(".header-menu"),
-		menuClose = document.querySelector(".menu__close"),
-		bodyScroll = document.querySelector("body"),
-		htmlScroll = document.querySelector("html");
+		menuClose = document.querySelector(".menu__close");
 
 	hamburger.addEventListener("click", () => {
 		menu.classList.toggle("menu_active");
-		bodyScroll.style.overflow = "hidden";
-		htmlScroll.style.overflow = "hidden";
-		// bodyScroll.classList.toggle("lock-scroll");
 	});
 
 	menuClose.addEventListener("click", () => {
 		menu.classList.toggle("menu_active");
-		bodyScroll.style.overflow = "";
-		htmlScroll.style.overflow = "";
-		// bodyScroll.classList.toggle("lock-scroll");
 	});
 
 	menuItem.forEach((item) => {
 		item.addEventListener("click", () => {
 			menu.classList.toggle("menu_active");
-			bodyScroll.style.overflow = "hidden";
-			htmlScroll.style.overflow = "hidden";
-			// bodyScroll.classList.toggle("lock-scroll");
 		});
 	});
 });
